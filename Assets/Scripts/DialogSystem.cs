e31595ac8e8e8eff06d54d9ec8c28dfd9f7d630a
Bernat Borràs Civil <70479573+BernatBC@users.noreply.github.com>
2024-12-07 10:32:04 +0100

    Add animations

--- Assets/Scripts/DialogSystem.cs
+++ Assets/Scripts/DialogSystem.cs
@@ -12,6 +12,9 @@ public class DialogSystem : MonoBehaviour
 
     private ARRaycastManager _raycastManager;
     private List<ARRaycastHit> _hits = new List<ARRaycastHit>();
+    public Animator animator;
+
+    private bool freezeInteraction = false;
 
 
     private void Start()
@@ -21,7 +24,7 @@ public class DialogSystem : MonoBehaviour
 
     private void Update()
     {
-        if (Input.touchCount == 1)
+        if (!freezeInteraction && Input.touchCount == 1)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
             if (Physics.Raycast(ray, out RaycastHit hit))
@@ -38,22 +41,31 @@ public class DialogSystem : MonoBehaviour
 
     public void Option1Selected() {
         Debug.Log("Option 1 selected");
-        HideCanvas();
+        StartCoroutine(CookCoroutine());
+        // Show dish model 1
     }
 
     public void Option2Selected()
     {
         Debug.Log("Option 2 selected");
-        HideCanvas();
+        StartCoroutine(CookCoroutine());
+        // Show dish model 2
     }
 
     public void Option3Selected()
     {
         Debug.Log("Option 3 selected");
-        HideCanvas();
+        StartCoroutine(CookCoroutine());
+        // Show dish model 3
     }
 
-    private void HideCanvas() {
+    IEnumerator CookCoroutine()
+    {
         canvas.SetActive(false);
+        freezeInteraction = true;
+        animator.SetBool("IsCooking", true);
+        yield return new WaitForSeconds(10);
+        animator.SetBool("IsCooking", false);
+        freezeInteraction = false;
     }
 }
